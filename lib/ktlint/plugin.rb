2e7fa1c65d581b785612064704379a40a38ad9a9
Takuma Homma <nagomimatcha@gmail.com>
2018-11-11 22:40:35 +0900

    Enable to send markdown comment

--- lib/ktlint/plugin.rb
+++ lib/ktlint/plugin.rb
@@ -4,6 +4,10 @@ module Danger
   class DangerKtlint < Plugin
     attr_accessor :filtering
 
+    # Run ktlint task using command line interface
+    # Will fail if `ktlint` is not installed
+    # Skip lint task if files changed are empty
+    # @return [void]
     def lint(inline_mode: false)
       unless ktlint_exists?
         fail("Couldn't find ktlint command. Install first.")
@@ -13,8 +17,22 @@ module Danger
       target = git.added_files + git.modified_files
       return if target.empty?
 
-      result = JSON.parse(`ktlint #{target.join(' ')} --reporter=json`)
-      return if result.empty?
+      results = JSON.parse(`ktlint #{target.join(' ')} --reporter=json --relative`)
+      return if results.empty?
+
+      if inline_mode
+        # TODO: Send inline comment
+      else
+        send_markdown_comment(results)
+      end
+    end
+
+    def send_markdown_comment(results)
+      results.each {|result|
+        result['errors'].each {|error|
+          markdown(file: result['file'], line: error['line'], message: error['message'])
+        }
+      }
     end
 
     private
