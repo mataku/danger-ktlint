125106e79b1290ef18134136f4eb65e771187b1f
Takuma Homma <nagomimatcha@gmail.com>
2018-11-11 19:56:14 +0900

    Fail if ktlint is not installed

--- lib/ktlint/plugin.rb
+++ lib/ktlint/plugin.rb
@@ -1,33 +1,18 @@
 module Danger
-  # This is your plugin class. Any attributes or methods you expose here will
-  # be available from within your Dangerfile.
-  #
-  # To be published on the Danger plugins site, you will need to have
-  # the public interface documented. Danger uses [YARD](http://yardoc.org/)
-  # for generating documentation from your plugin source, and you can verify
-  # by running `danger plugins lint` or `bundle exec rake spec`.
-  #
-  # You should replace these comments with a public description of your library.
-  #
-  # @example Ensure people are well warned about merging on Mondays
-  #
-  #          my_plugin.warn_on_mondays
-  #
-  # @see  Takuma Homma/danger-ktlint
-  # @tags monday, weekends, time, rattata
-  #
   class DangerKtlint < Plugin
+    attr_accessor :filtering
 
-    # An attribute that you can read/write from your Dangerfile
-    #
-    # @return   [Array<String>]
-    attr_accessor :my_attribute
+    def lint(inline_mode: false)
+      unless ktlint_exists?
+        fail("Couldn't find ktlint command. Install first.")
+        return
+      end
+    end
 
-    # A method that you can call from your Dangerfile
-    # @return   [Array<String>]
-    #
-    def warn_on_mondays
-      warn 'Trying to merge code on a Monday' if Date.today.wday == 1
+    private
+
+    def ktlint_exists?
+      system 'which ktlint > /dev/null 2>&1' 
     end
   end
 end
